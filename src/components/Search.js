c1a88fea87a8d4ccfa2a54cbddd11ec27a73861a
Eric Tran <erictran1547@gmail.com>
2024-07-09 09:43:52 -0700

    Refactored some code into more components or into their more appropiate components

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,6 +7,7 @@ import {
 } from "../common.js";
 
 import renderError from "./Error.js";
+import renderJobList from "./JobList.js";
 import renderSpinner from "./Spinner.js";
 
 const submitHandler = (event) => {
@@ -55,29 +56,7 @@ const submitHandler = (event) => {
       numberEl.textContent = jobItems.length;
 
       // render job items in search job list
-      jobItems.slice(0, 7).forEach((jobItem) => {
-        const newJobItemHTML = `
-            <li class="job-item">
-                <a class="job-item__link" href="${jobItem.id}">
-                    <div class="job-item__badge">${jobItem.badgeLetters}</div>
-                    <div class="job-item__middle">
-                        <h3 class="third-heading">${jobItem.title}</h3>
-                        <p class="job-item__company">${jobItem.company}</p>
-                        <div class="job-item__extras">
-                            <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i>${jobItem.duration}</p>
-                            <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i>${jobItem.salery}</p>
-                            <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i>"${jobItem.location}</p>
-                        </div>
-                    </div>
-                    <div class="job-item__right">
-                        <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
-                        <time class="job-item__time">${jobItem.daysAgo}</time>
-                    </div>
-                </a>
-            </li>
-          `;
-        jobListSearchEl.insertAdjacentHTML("beforeend", newJobItemHTML);
-      });
+      renderJobList(jobItems);
     })
     .catch((error) => console.log(error));
 };
